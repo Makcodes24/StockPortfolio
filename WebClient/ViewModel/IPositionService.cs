889111a3c75dda0c3c80fb1a40d8a10ff3655c92
Makcodes24 <anilsq@gmail.com>
2024-11-21 22:56:36 -0500

    Add files via upload

---
+++ WebClient/ViewModel/IPositionService.cs
@@ -0,0 +1,13 @@
+﻿using WebClient.Model;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace WebClient.ViewModel
+{
+    public interface IPositionService
+    {
+        Task<IEnumerable<Position>> CalibratePortfolioAsync(List<Position> positions);
+        Task<IEnumerable<Position>> GetDesiredPositionAsync(Position position);
+    }
+}
